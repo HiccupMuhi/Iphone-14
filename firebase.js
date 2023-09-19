00f90d3f74e19849ba836cdb8e1906a95c0368f1
Mugilan <mugilanm@titan.co.in>
2023-09-19 09:45:22 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAOd891kG1AGMJfhirJliyKfLY40JCN29Y",
+    authDomain: "iphone-14-3d-website.firebaseapp.com",
+    projectId: "iphone-14-3d-website",
+    storageBucket: "iphone-14-3d-website.appspot.com",
+    messagingSenderId: "836900509856",
+    appId: "1:836900509856:web:d2762acffd1a640c3511c5",
+    measurementId: "G-CG1WMMRPCJ"
+  };
